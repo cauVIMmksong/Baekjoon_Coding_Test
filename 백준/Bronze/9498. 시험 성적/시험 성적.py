b78fd27b2500d25ab2cc2d5c8c1b7d287f47c38b
Myungkeun Song <90034522+cauVIMmksong@users.noreply.github.com>
2023-09-13 22:22:27 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if score <= 100 and score >=90:
+    print("A")
+if score <= 89 and score >=80:
+    print("B")
+if score <= 79 and score >=70:
+    print("C")
+if score <= 69 and score >=60:
+    print("D")
+if score <60:
+    print("F")
